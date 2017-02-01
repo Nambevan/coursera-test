15744acd9ec36a4c4d4c7ae4c038184e777c81ae
Nambevan <mag_got@i.ua>
2017-02-01 23:37:00 +0200

    assigment1

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,22 @@
 (function(){
+   'use strict';
+   angular.module('LunchCheck',[])
+       .controller('LunchCheckController', function($scope){
+           $scope.dishes = "";
+           $scope.checkMessage = "";
+           $scope.checkLunchItems = function () {
 
-    'use strict';
+                var dishesCount =  $scope.dishes.split([,[]]).length;
 
-    angular.module('LunchCheck',[])
-
-        .controller('LunchCheckController', function($scope){
-
-            $scope.CheckIfTooMuch = function () {
-                var dishes = document.getElementById('lunch-menu').value;
-                var len = dishes.split([,[]]);
-                var x = document.getElementById('mss');
-                var dish = 0;
-                for( var item in len){
-                    dish = dish + 1;
-                }
-                if(dish>=4){
-                    x.innerHTML = "Too much!";
-                }else if(!dishes || 0 === dishes.length){
-                    x.innerHTML = "Please enter data first!";
-                }else{
-                    x.innerHTML = "Enjoy!";
-                }
-                console.log(len);
-            };
-        });
+                   if ($scope.dishes == "") {
+                       $scope.checkMessage = "Please enter data first!"
+                   }else if(dishesCount>=4) {
+                       $scope.checkMessage = "Too much!"
+                   }else{
+                       $scope.checkMessage = "Enjoy!"
+                   }
+                console.log(dishesCount);
+           }
+       });
 })();
+
